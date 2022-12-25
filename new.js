46f73a3635dcc96690845d9fab1cf0bf89ffd1c4
nikita <cgf122gmail.com>
2022-12-25 08:10:16 -0800

    firts commit

---
+++ new.js
@@ -0,0 +1,12 @@
+var x = +prompt('Введите первое число:')
+console.log(x);
+
+var y = +prompt('Введите второе число:');
+console.log(y);
+
+var z = +prompt('Введите третье число:')
+console.log(z);
+
+let result =(x + y + z) / 3
+alert('Среднее арифметическое чисел = ' + result)
+
